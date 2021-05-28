903241df684ac8d16912274540245613959fc640
Spencer Young <spencer.young@spyoung.com>
2021-05-28 09:28:45 -0700

    use levenshtein for faster matching

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='voice-commander',
-    version='0.0.1a',
+    version='0.0.2a',
     packages=['voice_commander'],
-    install_requires=['fuzzywuzzy', 'keyboard', 'easygui', 'pyaudio', 'SpeechRecognition'],
+    install_requires=['fuzzywuzzy', 'fuzzywuzzy[speedup]', 'keyboard', 'easygui', 'pyaudio', 'SpeechRecognition'],
     url='https://github.com/spyoungtech/voice-commander',
     license='MIT',
     author='Spencer Young',
